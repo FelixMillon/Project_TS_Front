4123ab76584c752fb6ee285311e91dbe4aacc2ee
Elone Maccioni <elonemacc@gmail.com>
2023-12-05 21:12:39 +0100

    Maj Card, Card categories

--- food/src/components/controllers/Card.vue
+++ food/src/components/controllers/Card.vue
@@ -1,31 +1,79 @@
 <template>
-    <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-        <img
-            alt="Office"
-            src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-            class="h-56 w-full object-cover"
-        />
+<div class="flex mb-12">
+        <a href="#" class="group relative block overflow-hidden w-1/3 mx-10">
+    <img
+        src="https://images.unsplash.com/photo-1599481238640-4c1288750d7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80"
+        alt=""
+        class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+    />
 
-        <div class="bg-white p-4 sm:p-6">
-            <time datetime="2022-10-10" class="block text-xs text-gray-500">
-            10th Oct 2022
-            </time>
+    <div class="relative border border-gray-100 bg-white p-6">
+        <span class="whitespace-nowrap bg-yellow-400 px-3 py-1.5 text-xs font-medium"> New </span>
 
-            <a href="#">
-            <h3 class="mt-0.5 text-lg text-gray-900">
-                How to position your furniture for positivity
-            </h3>
-            </a>
+        <h3 class="mt-4 text-lg font-medium text-gray-900">Robot Toy</h3>
 
-            <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
-            dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
-            sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
-            voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
-            Molestias explicabo corporis voluptatem?
-            </p>
-        </div>
-    </article>
+        <p class="mt-1.5 text-sm text-gray-700">$14.99</p>
+
+        <form class="mt-4">
+        <button
+            class="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105"
+        >
+            Add to Cart
+        </button>
+        </form>
+    </div>
+  </a>
+
+  <a href="#" class="group relative block overflow-hidden w-1/3 mx-10">
+    <img
+        src="https://images.unsplash.com/photo-1599481238640-4c1288750d7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80"
+        alt=""
+        class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+    />
+
+    <div class="relative border border-gray-100 bg-white p-6">
+        <span class="whitespace-nowrap bg-yellow-400 px-3 py-1.5 text-xs font-medium"> New </span>
+
+        <h3 class="mt-4 text-lg font-medium text-gray-900">Robot Toy</h3>
+
+        <p class="mt-1.5 text-sm text-gray-700">$14.99</p>
+
+        <form class="mt-4">
+        <button
+            class="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105"
+        >
+            Add to Cart
+        </button>
+        </form>
+    </div>
+  </a>
+
+  <a href="#" class="group relative block overflow-hidden w-1/3 mx-10">
+    <img
+        src="https://images.unsplash.com/photo-1599481238640-4c1288750d7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80"
+        alt=""
+        class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+    />
+
+    <div class="relative border border-gray-100 bg-white p-6">
+        <span class="whitespace-nowrap bg-yellow-400 px-3 py-1.5 text-xs font-medium"> New </span>
+
+        <h3 class="mt-4 text-lg font-medium text-gray-900">Robot Toy</h3>
+
+        <p class="mt-1.5 text-sm text-gray-700">$14.99</p>
+
+        <form class="mt-4">
+        <button
+            class="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105"
+        >
+            Add to Cart
+        </button>
+        </form>
+    </div>
+  </a>
+</div>
+
+  
 </template>
 
 <script setup lang="ts">
